835143c57fe29143a618c09ec88a97ec92e5aa5b
Julien Schueller <schueller@phimeca.com>
2022-10-13 11:50:34 +0200

    Update to 6.x

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,125 +1,34 @@
-set -exou
+#!/bin/sh
 
-if [[ $(arch) == "aarch64" || $(uname) == "Darwin" ]]; then
-pushd qtwebengine-chromium
-
-# Ensure that Chromium is built using the correct sysroot in Mac
-awk 'NR==77{$0="    rebase_path(\"'$CONDA_BUILD_SYSROOT'\", root_build_dir),"}1' chromium/build/config/mac/BUILD.gn > chromium/build/config/mac/BUILD.gn.tmp
-rm chromium/build/config/mac/BUILD.gn
-mv chromium/build/config/mac/BUILD.gn.tmp chromium/build/config/mac/BUILD.gn
-
-git config user.name 'Anonymous'
-git config user.email '<>'
-
-git add -A
-git commit -m "Patches"
-popd
+LINUX_ON=ON
+if test `uname` = "Darwin"
+then
+  LINUX_ON=OFF
 fi
 
-git submodule init
-git submodule set-url src/3rdparty "$SRC_DIR"/qtwebengine-chromium
-git submodule set-branch --branch 87-based src/3rdparty
-git submodule update
-
-pushd src/3rdparty
-git checkout 87-based
-git pull
-popd
-
-mkdir qtwebengine-build
-pushd qtwebengine-build
-
-USED_BUILD_PREFIX=${BUILD_PREFIX:-${PREFIX}}
-echo USED_BUILD_PREFIX=${BUILD_PREFIX}
-
-# qtwebengine needs python 2
-mamba create --yes --prefix "${SRC_DIR}/python2_hack" --channel conda-forge --no-deps python=2
-export PATH=${SRC_DIR}/python2_hack/bin:${PATH}
-
-if [[ $(uname) == "Linux" ]]; then
-    ln -s ${GXX} g++ || true
-    ln -s ${GCC} gcc || true
-    ln -s ${USED_BUILD_PREFIX}/bin/${HOST}-gcc-ar gcc-ar || true
-
-    export LD=${GXX}
-    export CC=${GCC}
-    export CXX=${GXX}
-
-    chmod +x g++ gcc gcc-ar
-    export PATH=$PREFIX/bin:${PWD}:${PATH}
-
-    which pkg-config
-    export PKG_CONFIG_EXECUTABLE=$(which pkg-config)
-    export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig/:$BUILD_PREFIX/lib/pkgconfig/
-
-    # Set QMake prefix to $PREFIX
-    qmake -set prefix $PREFIX
-
-    qmake QMAKE_LIBDIR=${PREFIX}/lib \
-        QMAKE_LFLAGS+="-Wl,-rpath,$PREFIX/lib -Wl,-rpath-link,$PREFIX/lib -L$PREFIX/lib" \
-        INCLUDEPATH+="${PREFIX}/include" \
-        PKG_CONFIG_EXECUTABLE=$(which pkg-config) \
-        ..
-
-    #cat config.log
-    #exit 1
-    CPATH=$PREFIX/include:$BUILD_PREFIX/src/core/api make -j$CPU_COUNT
-    make install
+if test "$CONDA_BUILD_CROSS_COMPILATION" = "1"
+then
+  CMAKE_ARGS="${CMAKE_ARGS} -DQT_HOST_PATH=${BUILD_PREFIX}"
 fi
 
-if [[ $(uname) == "Darwin" ]]; then
-    # Let Qt set its own flags and vars
-    for x in OSX_ARCH CFLAGS CXXFLAGS LDFLAGS
-    do
-        unset $x
-    done
+cmake -LAH -G "Ninja" ${CMAKE_ARGS} \
+  -DCMAKE_PREFIX_PATH=${PREFIX} \
+  -DCMAKE_FIND_FRAMEWORK=LAST \
+  -DCMAKE_INSTALL_RPATH:STRING=${PREFIX}/lib \
+  -DCMAKE_MESSAGE_LOG_LEVEL=STATUS \
+  -DINSTALL_BINDIR=lib/qt6/bin \
+  -DINSTALL_PUBLICBINDIR=usr/bin \
+  -DINSTALL_LIBEXECDIR=lib/qt6 \
+  -DINSTALL_DOCDIR=share/doc/qt6 \
+  -DINSTALL_ARCHDATADIR=lib/qt6 \
+  -DINSTALL_DATADIR=share/qt6 \
+  -DINSTALL_INCLUDEDIR=include/qt6 \
+  -DINSTALL_MKSPECSDIR=lib/qt6/mkspecs \
+  -DINSTALL_EXAMPLESDIR=share/doc/qt6/examples \
+  -DPython3_EXECUTABLE=${BUILD_PREFIX}/bin/python \
+  -DFEATURE_webengine_system_ffmpeg=ON \
+  -DFEATURE_webengine_system_libevent=ON \
+  -DFEATURE_webengine_system_icu=ON \
+  -B build .
+cmake --build build --target install
 
-    # Qt passes clang flags to LD (e.g. -stdlib=c++)
-    export LD=${CXX}
-    export PATH=${PWD}:${PATH}
-
-    # Use xcode-avoidance scripts
-    export PATH=$PREFIX/bin/xc-avoidance:$PATH
-
-    export APPLICATION_EXTENSION_API_ONLY=NO
-
-    EXTRA_FLAGS=""
-    if [[ $(arch) == "arm64" ]]; then
-      EXTRA_FLAGS="QMAKE_APPLE_DEVICE_ARCHS=arm64"
-    fi
-    
-    if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
-      # The python2_hack does not know about _sysconfigdata_arm64_apple_darwin20_0_0, so unset the data name
-      unset _CONDA_PYTHON_SYSCONFIGDATA_NAME
-    fi
-
-    # Set QMake prefix to $PREFIX
-    qmake -set prefix $PREFIX
-
-    # sed -i '' -e 's/-Werror//' $PREFIX/mkspecs/features/qt_module_headers.prf
-
-    qmake QMAKE_LIBDIR=${PREFIX}/lib \
-        INCLUDEPATH+="${PREFIX}/include" \
-        CONFIG+="warn_off" \
-        QMAKE_CFLAGS_WARN_ON="-w" \
-        QMAKE_CXXFLAGS_WARN_ON="-w" \
-        QMAKE_CFLAGS+="-Wno-everything" \
-        QMAKE_CXXFLAGS+="-Wno-everything" \
-        $EXTRA_FLAGS \
-        QMAKE_LFLAGS+="-Wno-everything -Wl,-rpath,$PREFIX/lib -L$PREFIX/lib" \
-        PKG_CONFIG_EXECUTABLE=$(which pkg-config) \
-        ..
-
-    # find . -type f -exec sed -i '' -e 's/-Wl,-fatal_warnings//g' {} +
-    # sed -i '' -e 's/-Werror//' $PREFIX/mkspecs/features/qt_module_headers.prf
-
-    make -j$CPU_COUNT
-    make install
-fi
-
-# Post build setup
-# ----------------
-# Remove static libraries that are not part of the Qt SDK.
-pushd "${PREFIX}"/lib > /dev/null
-    find . -name "*.a" -and -not -name "libQt*" -exec rm -f {} \;
-popd > /dev/null
